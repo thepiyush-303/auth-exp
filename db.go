0c1f1f684725dca64c3f807948404fc22f3761df
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2025-12-24 14:18:37 +0530

    postgres_db

--- db.go
+++ db.go
@@ -9,7 +9,7 @@ import (
 func connectDB() *sql.DB{
 	db, err := sql.Open(
 		"pgx",
-		"postgres://postgres:heythisismypassword@localhost:5432/",
+		"postgres://postgres:heythisismypassword@localhost:5432/authdb",
 	)
 	if err != nil{
 		log.Fatal(err)
@@ -19,4 +19,50 @@ func connectDB() *sql.DB{
 	}
 	fmt.Println("working")
 	return db
+}
+
+
+func fetchUser(db *sql.DB, pk int) User {
+	query := `SELECT name, email, password, active FROM users WHERE id = $1`
+
+	var user User
+
+	err := db.QueryRow(query, pk).Scan(&user.Name, &user.Email, &user.Password, &user.Active)
+
+	if err != nil{
+		if err == sql.ErrNoRows{
+			fmt.Printf("No row with the id %d", pk)
+		}
+		log.Fatal(err)
+	}
+	return user
+}
+
+
+func insertUser(db *sql.DB, data User) int{
+	query := `INSERT INTO users	(
+		name, email, password, active)
+		VALUES ($1, $2, $3, $4) RETURNING id`
+
+	var pk int
+	err := db.QueryRow(query, data.Name, data.Email, data.Password, data.Active).Scan(&pk)
+
+	if err != nil{
+		log.Fatal(err)
+	}
+	return pk
+}
+
+func createUserTable(db *sql.DB){
+	query := `CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) UNIQUE NOT NULL,
+		email VARCHAR(100) UNIQUE NOT NULL,
+		password VARCHAR(100) NOT NULL,
+		active BOOLEAN
+	)`
+	_, err := db.Exec(query)
+	if err != nil{
+		log.Fatal(err)
+	}
 }
--- main.go
+++ main.go
@@ -2,69 +2,50 @@ package main
 
 import (
 	// "encoding/json"
-	"net/http"
-	"database/sql"
-	"golang.org/x/crypto/bcrypt"
-	_ "github.com/jackc/pgx/v5/stdlib"
-	)
+	"fmt"
+	// "net/http"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
+	// "golang.org/x/crypto/bcrypt"
+)
+
+
+type User struct{
+	Name string `json: name`
+	Email string `json: email`
+	Password string `json: password`
+	Active bool `json: active`
+}
 
 func main(){
 
 	db := connectDB()
-	mux := http.NewServeMux()
+	defer db.Close()
+	
+	createUserTable(db)
 
-	mux.HandleFunc("/", signupHandler(db))
+	data := User{
+		Name: "Piyush3",
+		Email: "piyush3@gmail.com",
+		Password: "secretpassword2",
+		Active: true,
+	}
+
+	pk := insertUser(db, data)
+
+	var fetchedUser User
+
+	fetchedUser = fetchUser(db, pk)
+	fmt.Printf("User -> %s", fetchedUser.Name)
+
+	// fmt.Printf("Id %d", pk)
+
+	// mux := http.NewServeMux()
+
+
+	// mux.HandleFunc("/", )
 	// mux. HandleFunc("POST /register", createUser)
 	// mux.HandleFunc("GET /login", getUser)
-	http.ListenAndServe(":3000", mux)
+	// http.ListenAndServe(":3000", mux)
 
 }
-
-
-func signupHandler(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		email := r.FormValue("email")
-		password := r.FormValue("password")
-
-		if email == "" || password == "" {
-			http.Error(w, "missing fields", http.StatusBadRequest)
-			return
-		}
-
-		hashedPassword, err := bcrypt.GenerateFromPassword(
-			[]byte(password),
-			bcrypt.DefaultCost,
-		)
-		if err != nil {
-			http.Error(w, "server error", http.StatusInternalServerError)
-			return
-		}
-
-		_, err = db.Exec(
-			"INSERT INTO users (email, password) VALUES ($1, $2)",
-			email,
-			string(hashedPassword),
-		)
-		if err != nil {
-			http.Error(w, "user already exists", http.StatusBadRequest)
-			return
-		}
-
-		w.Write([]byte("signup successful"))
-	}
-}
-
-
-// func createUser(w http.ResponseWriter, r *http.Request){
-
-// }
-
-// func getUser(w http.ResponseWriter, r *http.Request){
-
-// }
